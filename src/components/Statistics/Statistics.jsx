7f6b2d4b1c2bd8adf0c6462bcd06cddffdc44907
Lika <lika.rossinskaya@gmal.com>
2022-07-26 08:29:03 +0300

    renderind condition in App added

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,26 +2,22 @@ import PropTypes from 'prop-types';
 import styles from './Statistics.module.css';
 
 export function Statistics({ good, bad, neutral, total, positivePercentage }) {
-  if (total > 0) {
-    return (
-      <>
-        <h2 className={styles.statisticsTitle}>Statistics</h2>
-        <div className={styles.spanWrap}>
-          <span className={(styles.span, styles.statGood)}>Good: {good}</span>
-          <span className={(styles.span, styles.statNeutral)}>
-            Neutral: {neutral}
-          </span>
-          <span className={(styles.span, styles.statBad)}>Bad: {bad}</span>
-          <span className={(styles.span, styles.statTotal)}>
-            Total: {total}{' '}
-          </span>
-          <span className={(styles.span, styles.statPercetage)}>
-            Positive feedback: {positivePercentage} %
-          </span>
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <h2 className={styles.statisticsTitle}>Statistics</h2>
+      <div className={styles.spanWrap}>
+        <span className={(styles.span, styles.statGood)}>Good: {good}</span>
+        <span className={(styles.span, styles.statNeutral)}>
+          Neutral: {neutral}
+        </span>
+        <span className={(styles.span, styles.statBad)}>Bad: {bad}</span>
+        <span className={(styles.span, styles.statTotal)}>Total: {total} </span>
+        <span className={(styles.span, styles.statPercetage)}>
+          Positive feedback: {positivePercentage} %
+        </span>
+      </div>
+    </>
+  );
 }
 
 Statistics.propTypes = {
